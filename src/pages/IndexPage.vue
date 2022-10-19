79f142e8f933e514c96fd2acfa6f065f7039760f
Jonas Freitas <admin@jonasfreitas.dev>
2022-10-19 10:57:35 -0300

    Commit inicial

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="row items-center justify-evenly"> </q-page>
+</template>
+
+<script setup lang="ts">
+import { Contato } from "components/models";
+//import ExampleComponent from 'components/ExampleComponent.vue';
+import { ref } from "vue";
+
+const contatos = ref<Contato[]>([]);
+</script>
